5203adb0609839245df76a92ab0c7836fbcabfc4
Tijl-bit <tijl@sinon.nl>
2025-04-11 19:15:28 +0200

    twee verschillende controller classes gemaakt voor overzicht

--- API/API/Controllers/WeatherForecastController.cs
+++ API/API/Controllers/WeatherForecastController.cs
@@ -1,33 +1,51 @@
+using API.Models;
+using API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("api/objects")]
+    public class ObjectController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly IObject2DRepository _objectRepo;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ObjectController(IObject2DRepository objectRepo)
         {
-            _logger = logger;
+            _objectRepo = objectRepo;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Object2D>>> GetObjects()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(await _objectRepo.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Object2D>> GetObject(Guid id)
+        {
+            var obj = await _objectRepo.GetByIdAsync(id);
+            return obj == null ? NotFound() : Ok(obj);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateObject(Object2D object2D)
+        {
+            var id = await _objectRepo.InsertAsync(object2D);
+            return CreatedAtAction(nameof(GetObject), new { id }, id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateObject(Guid id, Object2D object2D)
+        {
+            if (id != object2D.Id) return BadRequest();
+            return await _objectRepo.UpdateAsync(object2D) ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteObject(Guid id)
+        {
+            return await _objectRepo.DeleteAsync(id) ? NoContent() : NotFound();
         }
     }
 }
