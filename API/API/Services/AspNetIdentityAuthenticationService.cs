a6c4a3701dff551c723e2919e2c953c79238c29c
Tijl-bit <tijl@sinon.nl>
2025-04-12 01:35:23 +0200

    testing secure backend for deployment

---
+++ API/API/Services/AspNetIdentityAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Services
+{
+    public class AspNetIdentityAuthenticationService : IAuthenticationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AspNetIdentityAuthenticationService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? GetCurrentAuthenticatedUserId()
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
