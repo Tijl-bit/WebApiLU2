db1460377ab7b024326a0866e33e717fc55a29fe
Tijl-bit <tijl@sinon.nl>
2025-04-11 17:24:23 +0200

    dotnet versie veranderd

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d96e5e9d200e9d7d2be998e61b11538e8a8ec03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef53f79540efef4bf26b2f431bf060b0298821d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
