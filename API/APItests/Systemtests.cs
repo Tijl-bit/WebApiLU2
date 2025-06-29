13d6614cc7e0a2b50d0269b406b6d2b919fa78c7
Tijl-bit <tijl@sinon.nl>
2025-06-29 02:32:40 +0200

    added system tests

---
+++ API/APItests/Systemtests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace APItests
+{
+    [TestClass]
+    public class EnvironmentSystemTests
+    {
+        private HttpClient _client;
+        private string _baseUrl = "https://tijlswebsite-dygxf0d5dcehd9fg.northeurope-01.azurewebsites.net";
+        private string _token;
+
+        [TestInitialize]
+        public async Task Setup()
+        {
+            _client = new HttpClient();
+            _token = await LoginAndGetToken();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+        }
+
+        private async Task<string> LoginAndGetToken()
+        {
+            var loginUrl = $"{_baseUrl}/accounts/login";
+            var loginPayload = new
+            {
+                email = "SystemTestsUser123@mail.com",
+                password = "SystemTestsUser123!",
+                twoFactorCode = "string",
+                twoFactorRecoveryCode = "string"
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(loginPayload), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync(loginUrl, content);
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(jsonString);
+
+            // Assuming the token is returned as json.token or json.accessToken
+            return json.token ?? json.accessToken;
+        }
+
+        [TestMethod]
+        public async Task CreateEnvironment_ReturnsCreated()
+        {
+            var url = $"{_baseUrl}/api/environments";
+            var newEnv = new
+            {
+                Name = "SystemTestEnv",
+                MaxHeight = 5,
+                MaxLength = 5
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(newEnv), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync(url, content);
+
+            Assert.IsTrue(response.IsSuccessStatusCode, $"Status Code: {response.StatusCode}");
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseData));
+            // You can add more validation here if needed
+        }
+
+        [TestMethod]
+        public async Task GetEnvironments_ReturnsOk()
+        {
+            var url = $"{_baseUrl}/api/environments";
+
+            var response = await _client.GetAsync(url);
+
+            Assert.IsTrue(response.IsSuccessStatusCode, $"Status Code: {response.StatusCode}");
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseData));
+            // Optionally deserialize and assert specific data
+        }
+    }
+}
